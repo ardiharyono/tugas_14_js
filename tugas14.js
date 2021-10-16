41d8f4071b02cfdd385470a29e66ebbf4f2e467b
Ardi Haryono <ardiharyono26@gmail.com>
2021-10-16 17:28:45 +0700

    Tugas 14 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas14.js
@@ -0,0 +1,5 @@
+var x = 37 / 26;
+console.log("hasilnya adalah = " + x);
+console.log("hasil ceil = " + Math.ceil(x));
+console.log("hasil floor = " + Math.floor(x));
+console.log("hasil round = " + Math.round(x));
